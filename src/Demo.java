ce3dd8d6652f527a3516f70b4bcc883c63d7aa4f
Ashutosh Salunkhe <https://github.com/ASHUTOSH-SALUNKHE/java-programs>
2026-02-13 10:36:45 +0530

    Day 1 Exercise

---
+++ src/Demo.java
@@ -0,0 +1,16 @@
+
+
+	
+public class Demo {
+		
+	public static void main(String[] args) {
+			
+			System.out.println("Name : Ashutosh Narendra Salunkhe");
+			System.out.println("Age : 21");
+			System.out.println("Address : Savarkar Colony , Sangli");
+			System.out.println("Education : Btech In Electrical Engineering");	
+			
+	}
+			
+}
+
